c2f732a8dd87b6116193904a81a07661b45df859
Jeff Thompson <jefft0@remap.ucla.edu>
2016-09-08 12:03:45 -0700

    unit-tests: Added test-aes-algorithm.

---
+++ make-unit-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Concatenate mocha.nut and all unit tests and put in bin/unit-tests.app.nut .
+mkdir -p bin
+./make-ndn-squirrel.sh
+cat ndn-squirrel.nut \
+  contrib/aes-squirrel/aes.class.nut \
+  tests/unit-tests/mocha.nut \
+  tests/unit-tests/test-aes-algorithm.nut \
+  > bin/unit-tests.app.nut
